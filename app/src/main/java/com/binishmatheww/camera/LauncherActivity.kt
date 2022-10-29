0ef37d4dc1dd9f80ba8e2fe9147d82828b10caa4
binishmatheww <binishmatheww@gmail.com>
2022-10-29 15:09:17 +0530

    Added option to select a CameraProp from available CameraProps. (Work in progress.)

--- app/src/main/java/com/binishmatheww/camera/LauncherActivity.kt
+++ app/src/main/java/com/binishmatheww/camera/LauncherActivity.kt
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults.buttonColors
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.binishmatheww.camera.composables.CameraPreviewLayout
 import com.binishmatheww.camera.composables.rememberCameraController
+import com.binishmatheww.camera.utils.log
 import kotlinx.coroutines.launch
 
 class LauncherActivity : AppCompatActivity() {
@@ -27,15 +33,50 @@ class LauncherActivity : AppCompatActivity() {
             ) {
 
                 val (
+                cameraPropsConstraint,
                 cameraPreviewLayoutConstraint,
                 captureButtonConstraint
                 ) = createRefs()
 
                 val cameraController = rememberCameraController()
 
-                val coroutineScope = rememberCoroutineScope()
+                val availableCameraProps by cameraController.availableCameraPropsFlow.collectAsState()
+
+                val selectedCameraProp by cameraController.selectedCameraPropFlow.collectAsState()
 
                 var isCaptureButtonEnabled by remember { mutableStateOf(true) }
+
+                LazyRow(
+                    modifier = Modifier
+                        .constrainAs(cameraPropsConstraint){
+                            top.linkTo(parent.top)
+                            linkTo(start = parent.start, end = parent.end)
+                        },
+                   ){
+
+                    items(availableCameraProps){ cameraProp ->
+
+                        Button(
+                            modifier = Modifier
+                                .padding(
+                                    horizontal = 8.dp
+                                ),
+                            onClick = {
+                                cameraController.cameraScope.launch {
+                                    cameraController.selectCamera(cameraProp)
+                                    cameraController.setSize(cameraProp.outputSizes.firstOrNull())
+                                }
+                            },
+                            colors = buttonColors(
+                                contentColor = Color.Black,
+                                backgroundColor = if(cameraProp == selectedCameraProp) Color.Green else Color.Gray
+                            )
+                        ) {
+                            Text(text = cameraProp.formatName)
+                        }
+                    }
+
+                }
 
                 CameraPreviewLayout(
                     modifier = Modifier
@@ -53,13 +94,15 @@ class LauncherActivity : AppCompatActivity() {
                     },
                     enabled = isCaptureButtonEnabled,
                     onClick = {
-                        coroutineScope.launch {
+
+                        cameraController.cameraScope.launch {
 
                             isCaptureButtonEnabled = false
                             cameraController.captureImage()
                             isCaptureButtonEnabled = true
 
                         }
+
                     }
                 ) {
 
