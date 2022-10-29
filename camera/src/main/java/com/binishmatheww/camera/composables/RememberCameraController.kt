c940bc7e1943cedc1157df2fa983e6155fa97a4b
binishmatheww <binishmatheww@gmail.com>
2022-10-29 10:41:27 +0530

    Added DisposableEffect to cancel the CameraController.cameraCoroutineScope on dispose.

--- camera/src/main/java/com/binishmatheww/camera/composables/RememberCameraController.kt
+++ camera/src/main/java/com/binishmatheww/camera/composables/RememberCameraController.kt
@@ -1,15 +1,32 @@
 package com.binishmatheww.camera.composables
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.DisposableEffectResult
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import com.binishmatheww.camera.CameraController
+import kotlinx.coroutines.cancel
 
 @Composable
 fun rememberCameraController() : CameraController {
 
     val context = LocalContext.current
 
-    return remember{ CameraController(context) }
+    val cameraController = remember{ CameraController(context) }
+
+    DisposableEffect(
+        key1 = context,
+        effect = {
+
+            onDispose {
+                cameraController.session.stopRepeating()
+                cameraController.cameraCoroutineScope.cancel("Recomposition triggered.")
+            }
+
+        }
+    )
+
+    return cameraController
 
 }
