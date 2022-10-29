431687c25902ce2354c08707ca91455c3d42c047
binishmatheww <binishmatheww@gmail.com>
2022-10-29 15:06:37 +0530

    Moved camera initialization part to the CameraController.

--- camera/src/main/java/com/binishmatheww/camera/composables/CameraPreviewLayout.kt
+++ camera/src/main/java/com/binishmatheww/camera/composables/CameraPreviewLayout.kt
@@ -1,17 +1,11 @@
 package com.binishmatheww.camera.composables
 
-import android.hardware.camera2.CameraDevice
-import android.media.ImageReader
 import android.view.SurfaceHolder
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import com.binishmatheww.camera.CameraController
 import com.binishmatheww.camera.utils.AutoFitSurfaceView
-import com.binishmatheww.camera.utils.IMAGE_BUFFER_SIZE
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 @Composable
 fun CameraPreviewLayout(
@@ -23,9 +17,9 @@ fun CameraPreviewLayout(
         modifier = modifier,
         factory = { con ->
 
-            val viewFinder = AutoFitSurfaceView(con)
+            cameraController.viewFinder = AutoFitSurfaceView(con)
 
-            viewFinder.holder.addCallback(object : SurfaceHolder.Callback {
+            cameraController.viewFinder?.holder?.addCallback(object : SurfaceHolder.Callback {
 
                 override fun surfaceDestroyed(
                     holder: SurfaceHolder
@@ -40,67 +34,13 @@ fun CameraPreviewLayout(
 
                 override fun surfaceCreated(holder: SurfaceHolder) {
 
-                    cameraController.cameraScope.launch {
-
-                        val sizes = cameraController.selectedCameraProp?.outputSizes ?: emptyList()
-
-                        val size = sizes.maxByOrNull { it.height * it.width } ?: sizes.first()
-
-                        withContext(Dispatchers.Main){
-
-                            viewFinder.setAspectRatio(
-                                size.width,
-                                size.height
-                            )
-
-                        }
-
-                        // Open the selected camera
-                        cameraController.cameraDevice = CameraController.openCamera(
-                            cameraController = cameraController
-                        )
-
-                        // Initialize an image reader which will be used to capture still photos
-                        cameraController.imageReader = ImageReader.newInstance(
-                            size.width,
-                            size.height,
-                            cameraController.selectedCameraProp!!.formatId,
-                            IMAGE_BUFFER_SIZE
-                        )
-
-                        // Creates list of Surfaces where the camera will output frames
-                        cameraController.targets.clear()
-                        cameraController.targets.addAll(listOf(viewFinder.holder.surface, cameraController.imageReader!!.surface))
-
-                        // Start a capture session using our open camera and list of Surfaces where frames will go
-                        cameraController.cameraCaptureSession = CameraController.createCaptureSession(
-                            cameraController = cameraController
-                        )
-
-                        cameraController.cameraDevice?.createCaptureRequest(
-                            CameraDevice.TEMPLATE_PREVIEW
-                        )?.let { captureRequestBuilder ->
-
-                            captureRequestBuilder.addTarget(viewFinder.holder.surface)
-
-                            // This will keep sending the capture request as frequently as possible until the
-                            // session is torn down or session.stopRepeating() is called
-                            cameraController.cameraCaptureSession?.setRepeatingRequest(
-                                captureRequestBuilder.build(),
-                                null,
-                                cameraController.cameraHandler
-                            )
-
-
-                        }
-
-
-                    }
+                    cameraController.initialize()
 
                 }
+
             })
 
-            viewFinder
+            cameraController.viewFinder!!
 
         }
     )
